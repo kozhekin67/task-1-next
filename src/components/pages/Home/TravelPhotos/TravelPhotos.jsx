bd4c4e3eeb5f8223b3016b56285b06138eb276a4
KozhhekinAN <kozhekin67@gmail.com>
2024-10-20 17:50:12 +0300

    adding a photo

--- src/components/pages/Home/TravelPhotos/TravelPhotos.jsx
+++ src/components/pages/Home/TravelPhotos/TravelPhotos.jsx
@@ -3,7 +3,6 @@ import cx from 'classnames';
 import Image from 'next/image';
 import { string } from 'prop-types';
 
-// import reviews from 'src/stubs/reviews';
 import travelPhotosRowFirst from 'src/stubs/travelPhotosRowFirst';
 import travelPhotosRowSecond from 'src/stubs/travelPhotosRowSecond';
 import travelPhotosRowThird from 'src/stubs/travelPhotosRowThird';
@@ -23,10 +22,10 @@ const TravelPhotos = ({ className }) => {
       <div className={cx(s.container)}>
         {travelPhotosRowFirst.map(({ image, alt }) => (
           // eslint-disable-next-line react/jsx-key
-          <div className={cx(s.rowFirst)}>
+          <div className={cx(s.container__row, s.container__row_first)}>
             <Image
               src={image}
-              className={cx(s.rowFirst__photo)}
+              className={cx(s.container__photo)}
               width={1000}
               height={1000}
               alt={alt}
@@ -35,22 +34,22 @@ const TravelPhotos = ({ className }) => {
         ))}
         {travelPhotosRowSecond.map(({ image, alt }) => (
           // eslint-disable-next-line react/jsx-key
-          <div className={cx(s.rowSecond)}>
+          <div className={cx(s.container__row, s.container__row_second)}>
             <Image
               src={image}
-              className={cx(s.rowSecond__photo)}
+              className={cx(s.container__photo)}
               width={1000}
               height={1000}
               alt={alt}
             />
           </div>
         ))}
-        {travelPhotosRowFirst.map(({ image, alt }) => (
+        {travelPhotosRowThird.map(({ image, alt }) => (
           // eslint-disable-next-line react/jsx-key
-          <div className={cx(s.rowFirst)}>
+          <div className={cx(s.container__row, s.container__row_third)}>
             <Image
               src={image}
-              className={cx(s.rowFirst__photo)}
+              className={cx(s.container__photo)}
               width={1000}
               height={1000}
               alt={alt}
@@ -58,50 +57,6 @@ const TravelPhotos = ({ className }) => {
           </div>
         ))}
       </div>
-
-      {/* <div class="travel-photos__container container-photos">
-                    <div class="container-photos__row container-photos__row_first">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-first-row-1.jpeg" alt="travel-photos-first-row-1">
-                    </div>    
-                    <div class="container-photos__row container-photos__row_first">    
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-first-row-2.png" alt="travel-photos-first-row-2">
-                    </div>    
-                    <div class="container-photos__row container-photos__row_first">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-first-row-3.png" alt="travel-photos-first-row-3">
-                    </div>    
-                    <div class="container-photos__row container-photos__row_first">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-first-row-4.jpeg" alt="travel-photos-first-row-4">
-                    </div>
-
-                    <div class="container-photos__row container-photos__row_second">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-second-row-1.png" alt="travel-photos-second-row-1">
-                    </div> 
-                    <div class="container-photos__row container-photos__row_second">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-second-row-2.png" alt="travel-photos-second-row-2">
-                    </div> 
-                    <div class="container-photos__row container-photos__row_second"> 
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-second-row-3.jpeg" alt="travel-photos-second-row-3">
-                    </div> 
-                    <div class="container-photos__row container-photos__row_second">   
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-second-row-4.png" alt="travel-photos-second-row-4">
-                    </div>  
-                    <div class="container-photos__row container-photos__row_second">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-second-row-5.png" alt="travel-photos-second-row-5">
-                    </div>
-        
-                    <div class="container-photos__row container-photos__row_third">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-third-row-1.png" alt="travel-photos-second-row-1">
-                    </div> 
-                    <div class="container-photos__row container-photos__row_third">
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-third-row-2.jpeg" alt="travel-photos-second-row-2">
-                    </div> 
-                    <div class="container-photos__row container-photos__row_third"> 
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-third-row-3.jpeg" alt="travel-photos-second-row-3">
-                    </div> 
-                    <div class="container-photos__row container-photos__row_third">   
-                        <img class="container-photos__travel-photo" src="img/travel-photos/travel-photos-third-row-4.jpeg" alt="travel-photos-second-row-4">
-                    </div> 
-            </div> */}
     </section>
   );
 };
